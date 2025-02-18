3432be7a96fc14a6e939d1541039739615a83ee3
Badri Hiriyur <bhiriyur@t2d2.ai>
2025-02-18 14:39:17 -0500

    feat: Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics mode="production" />;
+        <SpeedInsights />
       </body>
     </html>
   );
